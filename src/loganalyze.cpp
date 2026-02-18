5b3fac1d80aac976d25d725bd6607293017097bd
HabibAlShaikh <ihabibshaikh@gmail.com>
2026-02-18 21:44:56 +0300

    last update i can open files reads them and Filters them what i want with --level next step i need to handle --file files exestence. rejects unknown flags

--- src/loganalyze.cpp
+++ src/loganalyze.cpp
@@ -1,31 +1,70 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+void openLog();
+
+std::string filePath = "";
+std::string level = "";
 
 int main(int argc, char* argv[]) {
-    //std::cout << "Argument count: " << argc << "\n";
+	for(int i = 1; i < argc; i++) {
+		std::string arg = argv[i];
 
-    std::string filePath = "";
-    std::string level = "";
+		if(arg == "--path") {
+			if(i + 1 < argc) {
+				filePath = argv[++i];
+			} else {
+				std::cerr << "Error: --path requires a value" << std::endl;
+				return 1;
+			}
+		} else if (arg == "--level") {
+			if (i + 1 < argc) {
+				level = argv[++i];
+			} else {
+				std::cerr << "Error: --level requires a value" << std::endl;
+				return 1;
+			}
+		} else if (arg == "--help") {
+			std::cout << "Usage: ./loganalyze --path <path> [--level INFO|WARN|ERROR]" << std::endl;
+			return 0;
+		} else {
+			std::cerr << "Unknown argument: " << arg << std::endl;
+			return 1;
+		}
+	}
 
-    for(int i = 1; i < argc; i++) {
-	    std::string arg = argv[i];
-
-	    if(arg == "--path") {
-		if(i + 1 < argc) {
-			filePath = argv[++i];
-		}else {std::cerr << "Error: --file requires a value\n"; return 1;}
-	    }else if (arg == "--level") {
-	    	if (i + 1 < argc) {
-	    		level = argv[++i];
-	    	}else {std::cerr << "Error: --level requires a value\n"; return 1;}
-	    }else if (arg == "--help") {
-	    	std::cout << "Usage: ./loganalyze --file <path> [--level INFO|WARN|ERROR]\n";
-            	return 0;
-	    }else {
-	    	std::cerr << "Unkonwn argument: " << arg << std::endl;
+	if(filePath.empty()) {
+		std::cerr << "Error: --path is required" << std::endl;
 		return 1;
-	    }
-    }
+	}
 
-    return 0;
+	std::cout << "File: " << filePath << std::endl;
+	std::cout << "Level: " << level << std::endl;
+
+	openLog();
+
+	return 0;
+}
+
+void openLog () {
+	std::ifstream logFile(filePath);
+
+	if (!logFile.is_open()) {
+		std::cerr << "Could not open the log file: " << filePath << std::endl;
+		return;
+	} else {
+		std::cout << "File opened successfully!" << std::endl;
+	}
+
+	std::string line;
+	while (std::getline(logFile, line)) {
+		// If level is empty, it will match every line.
+		// If not empty, it filters by the provided string.
+		if (level.empty() || line.find(level) != std::string::npos) {
+			std::cout << line << std::endl;
+		}
+	}
+
+	logFile.close();
 }
